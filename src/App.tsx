0dee5b80a619d7a03bde8a4bb2e9dda3e9b23a27
Bogdan <kyshbogdan@gmail.com>
2024-06-17 19:20:06 +0300

    install storybook, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const App: React.FC = () => {
   return (
-    <h1>🍕 PizzaStack</h1>
+    <h1 className='font-bold text-3xl text-red-500'>🍕 PizzaStack</h1>
   );
 }
